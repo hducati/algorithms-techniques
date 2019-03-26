c46d22deab478de49494d7a0d3ad274f9a0c5826
Henrique Ducati Miranda <henrique.dct@gmail.com>
2019-03-26 16:35:28 -0300

    add example using pyod to detect outliers

---
+++ outlier_pyod.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from pyod.models.knn import KNN
+
+
+def main():
+    data = pd.read_csv('credit-data.csv')
+    data = data.dropna()
+    
+    detector = KNN()
+    detector.fit(data.iloc[:, 1:4])
+    
+    predict = detector.labels_
+    predict_trust = detector.decision_scores_
+    print(str(predict))
+    print(str(predict_trust))
+    
+    outlier = [i for i in range(len(predict)) if predict[i] == 1]
+    print(str(outlier))
+    
+    df_outlier = data.iloc[outlier, :]
+    print(df_outlier)
+    
+    
+if __name__ == '__main__':
+    main()
